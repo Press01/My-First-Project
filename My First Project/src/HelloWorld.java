203c21ccd21939cec5aac7530e97d00013482c97
Admin <Admin@ADMINPC>
2018-03-09 19:02:58 +0300

    HELLO***

--- My First Project/src/HelloWorld.java
+++ My First Project/src/HelloWorld.java
@@ -2,10 +2,16 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Horoshaya pogoda");
-		System.out.println("Uzhe gorazdo teplee");
-		System.out.println("Goodbye World");
+		System.out.println("*   * ***** *     *      ***");
+		System.out.println("*   * *     *     *     *   *");
+		System.out.println("*   * *     *     *     *   *");
+		System.out.println("*   * *     *     *     *   *");
+		System.out.println("***** ***** *     *     *   *");
+		System.out.println("*   * *     *     *     *   *");
+		System.out.println("*   * *     *     *     *   *");
+		System.out.println("*   * *     *     *     *   *");
+		System.out.println("*   * ***** ***** *****  ***");
+		System.out.println("");
 		// TODO Auto-generated method stub
 	}
 }
